8491dfd4a485c519eeef23d8838c743a2c224f1d
Vanessa <vanessalacerda219@gmail.com>
2025-11-04 21:38:32 -0300

    interface

---
+++ Interface.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.Qt import Qt
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtPrintSupport import *
+import os, sys
+
+from login import Ui_login
+
+class login(QDialog):
+    def __init__(self,*args,**argvs):
+        super(login,self).__init__(*args,**argvs)
+        self.ui = Ui_login()
+        self.ui.setupUi(self)
+
+app = QApplication(sys.argv)
+window = login()
+window.show()
+sys.exit(app.exec_())
